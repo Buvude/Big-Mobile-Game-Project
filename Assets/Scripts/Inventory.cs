b094b6b359c791769b7c72a7294819c040007824
Buvude <90856761+Buvude@users.noreply.github.com>
2023-05-02 15:47:45 -0400

    added some enums

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public enum inventoryItems {nothing, emptyWC, fullWC, axe, pileOfWood, torch, dynamite, litDynamite, dirtyGold, goldMedal  };
+    public inventoryItems iI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
